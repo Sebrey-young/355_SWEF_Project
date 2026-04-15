57ac489aacbc6784f8676d137e9a8f96a1fcb9de
iscrooble <dpeyman@vcu.edu>
2026-04-15 18:30:36 -0400

    Testing main page
    Also a showcase of what changes are reflected where

--- swe-project/src/app/page.js
+++ swe-project/src/app/page.js
@@ -5,34 +5,18 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
+{/*         <Image
           className={styles.logo}
           src="/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
           priority
-        />
+        /> */}
         <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
+          <h1>This is the start of our project!!!!</h1>
           <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
+          These are more words. aijfsiofjfojaiojjio
           </p>
         </div>
         <div className={styles.ctas}>
